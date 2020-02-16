b67406bd6a0903551acbbdcda856bc501935dfe1
Joyce Sun <joannasun@Joannas-MacBook-Pro.local>
2020-02-15 19:58:08 -0500

    test

---
+++ mysite/depop/views.py
@@ -0,0 +1,27 @@
+from django.http import HttpResponse
+import requests
+from bs4 import BeautifulSoup
+import json
+import pprint
+
+# Create your views here.
+def index(request, uname="depop"):
+    # assume ing we have a template
+    # we just say
+    # the template will have a variable for followers,
+    # and we just pass it that. the template will be defined somewhere else
+    # return renderTemplate(display_count(uname))
+    return HttpResponse("Hello, world. " + uname + " has " + display_count(uname) + " followers.")
+
+
+def display_count(u_name):
+    URL = 'https://www.depop.com/' + u_name + "/"
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    results = soup.find(id='__NEXT_DATA__')
+    d = json.loads(results.text)
+    try:
+        follower_count = d['props']['pageProps']['shop']['followers']
+        return str(follower_count)
+    except KeyError:
+        return "invalid username"
